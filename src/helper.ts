e99bb09c17e38423402a6ddf53033f6ee66b06e0
Mohandes Jiri <mohandes.jiri@gmail.com>
2022-10-23 02:33:47 +0330

    Fix

--- src/helper.ts
+++ src/helper.ts
@@ -1,38 +1,34 @@
-import {BigInt, Bytes, log} from "@graphprotocol/graph-ts";
+import {BigInt, Bytes} from "@graphprotocol/graph-ts";
 import {CompleteTransfer} from "../generated/schema";
 
 export function processSequenceNumber(vaa: Bytes, hash: Bytes) : void {
-	// log.info('Checking version', []);
 	const version = vaa[0];
 	if(version !== 1) {
 		return;
 	}
-	const x = Math.floor(Math.random() * 1000).toString();
-	// log.info('Version is OK 232130000', []);
-	const signatureLength = vaa[5];
-	log.error('{}: signatureLength: {}',[x, signatureLength.toString()]);
-	const headerEnd = 5 + signatureLength * 66;
-	log.error('{}: headerEnd: {}',[x, headerEnd.toString()]);
+	const signatureLength: u8 = vaa[5];
+	let signatures: i32 = 0;
+	for (let i: u8 = 0; i < signatureLength; i++) {
+		signatures = signatures + 66;
+	}
+	const headerEnd = 5 + signatures;
 	const sequenceStart = headerEnd + 42 + 1;
-	log.error('{}: sequenceStart: {}',[x, sequenceStart.toString()]);
 	const emitterChainStart = headerEnd + 8 + 1;
-	log.error('{}: emitterChainStart: {}',[x, emitterChainStart.toString()]);
 	const emitterChain =  new Bytes(2);
 	for (let i = 0; i < 2; i++) {
 		emitterChain[i] = vaa[emitterChainStart + i];
-		log.error('{}: emitterChain at {}: {}',[x, i.toString(), vaa[emitterChainStart + i].toString()]);
 	}
-	log.warning('Emitter chain: {}', [emitterChain.toHexString()]);
 	let emitterResult: u16 = 0;
 	emitterResult = (emitterResult | emitterChain[0]) << 8;
-	emitterResult = (emitterResult | emitterChain[1]) << 8;
+	emitterResult = emitterResult | emitterChain[1];
+	if (emitterResult !== 1) {
+		return;
+	}
 
 	const sequence =  new Bytes(8);
 	for (let i = 0; i < 8; i++) {
 		sequence[i] = vaa[sequenceStart + i];
 	}
-	// log.error('My value after that is: {}', [sequence.toHexString()]);
-	// log.info('My value after that is same : {}', [sequence.toHexString()]);
 	let result: u64 = 0
 	result = (result | sequence[0]) << 8;
 	result = (result | sequence[1]) << 8;
